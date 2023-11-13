4a551f4f45389d6112ce1642fe116c9b26060e90
GruiicK <gruiick@users.noreply.github.com>
2023-11-13 14:37:35 +0100

    propagate Chapter 8 to Main

--- pyRogue/entity_factories.py
+++ pyRogue/entity_factories.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 from components.ai import HostileEnemy
+from components.consumable import HealingConsumable
 from components.fighter import Fighter
-from entity import Actor
+from components.inventory import Inventory
+from entity import Actor, Item
 
 player = Actor(
     char="@",
@@ -10,6 +12,7 @@ player = Actor(
     name="Player",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=30, defense=2, power=5),
+    inventory=Inventory(capacity=26),
 )
 
 orc = Actor(
@@ -18,6 +21,7 @@ orc = Actor(
     name="Orc",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=10, defense=0, power=3),
+    inventory=Inventory(capacity=0),
 )
 
 troll = Actor(
@@ -26,4 +30,12 @@ troll = Actor(
     name="Troll",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=16, defense=1, power=4),
+    inventory=Inventory(capacity=0),
+)
+
+health_potion = Item(
+    char="!",
+    color=(127, 0, 255),
+    name="Health Potion",
+    consumable=HealingConsumable(amount=4),
 )
--- pyRogue/main.py
+++ pyRogue/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import copy
+import traceback
 import tcod
 
 import color
@@ -20,6 +21,7 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 30
     max_monsters_per_room = 2
+    max_items_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -36,6 +38,7 @@ def main() -> None:
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
+        max_items_per_room=max_items_per_room,
         engine=engine,
         )
 
@@ -58,7 +61,15 @@ def main() -> None:
             engine.event_handler.on_render(console=root_console)
             context.present(root_console)
 
-            engine.event_handler.handle_events(context)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+
+            except Exception:  # Handle exceptions in game.
+                traceback.print_exc()  # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 
 if __name__ == "__main__":
