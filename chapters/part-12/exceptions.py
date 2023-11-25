089b2b4beafe009b022285a6d173be3d6bb95ccf
GruiicK <gruiick@users.noreply.github.com>
2023-11-25 10:38:16 +0100

    Chapter 12 is done (was fast!)

---
+++ chapters/part-12/exceptions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Impossible(Exception):
+    """Exception raised when an action is impossible to be performed.
+
+    The reason is given as the exception message.
+    """
+
+
+class QuitWithoutSaving(SystemExit):
+    """Can be raised to exit the game without automatically saving."""
